283f30201a37e0281db57f15cfa029c9bb4374ba
hotsixisbest <babytaikoer>
2023-11-23 21:19:23 +0900

    Ver 1.1.5

--- src/checkLogin.js
+++ src/checkLogin.js
@@ -2,17 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const cheerio_1 = require("cheerio");
 function checkLogin(response) {
-    if (response?.data) {
-        let $ = (0, cheerio_1.load)(response.data);
-        if ($('form.login_form').length == 0) {
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-    else {
+    if (!response?.data) {
         return false;
     }
+    let $ = (0, cheerio_1.load)(response.data);
+    if ($('form#login_form').length !== 0) {
+        return false;
+    }
+    if ($('h1').html()?.trim() == 'カード登録 (1枚登録中)') {
+        return false;
+    }
+    if (!response?.config?.url) {
+        return false;
+    }
+    if (new URL(response.config.url).origin !== 'https://donderhiroba.jp') {
+        return false;
+    }
+    return true;
 }
 exports.default = checkLogin;
