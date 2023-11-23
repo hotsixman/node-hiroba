27e2e2d06dbfb60414db4df95ed8bde59ad95cd1
hotsixisbest <babytaikoer>
2023-11-23 21:18:09 +0900

    checkLogin 함수 수정

--- src/checkLogin.ts
+++ src/checkLogin.ts
@@ -1,18 +1,25 @@
 import { AxiosResponse } from 'axios';
-import {load} from 'cheerio';
+import { load } from 'cheerio';
 
-export default function checkLogin(response:AxiosResponse):boolean{
-    if(response?.data){
-        let $ = load(response.data);
+export default function checkLogin(response: AxiosResponse): boolean {
+    if (!response?.data) {
+        return false;
 
-        if($('form.login_form').length == 0){//loginform없음
-            return true;
-        }
-        else{//loginform있음
-            return false;
-        }
     }
-    else{
+    let $ = load(response.data);
+
+    if ($('form#login_form').length !== 0) {//loginform없음
         return false;
     }
+    if($('h1').html()?.trim() == 'カード登録 (1枚登録中)'){//카드 선택창
+        return false;
+    }
+    if(!response?.config?.url){//url이 없음
+        return false;
+    }
+    if(new URL(response.config.url).origin !== 'https://donderhiroba.jp'){//동더히로바가 아님
+        return false;
+    }
+
+    return true;
 }
