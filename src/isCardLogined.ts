add0312da28ab52ea067d7032f10e5d0d5b6684f
hotsixisbest <galactagun12@gmail.com>
2024-02-20 15:54:29 +0900

    isCardLogined 함수 생성

---
+++ src/isCardLogined.ts
@@ -0,0 +1,29 @@
+import { AxiosResponse } from 'axios';
+import { load } from 'cheerio';
+
+/**
+ * axios response를 인수로 받아 카드 로그인이 되어있는지 체크합니다.
+ * @param response
+ * @returns {boolean}
+ */
+export default function isLogined(response: AxiosResponse): boolean {
+    if (!response?.data) {
+        return false;
+    }
+    let $ = load(response.data);
+
+    if ($('form#login_form').length !== 0) {//loginform있음
+        return false;
+    }
+    if($('h1').html()?.trim() == 'カード登録 (1枚登録中)'){//카드 선택창일 때
+        return false;
+    }
+    if(!response?.config?.url){//url이 없음
+        return false;
+    }
+    if(new URL(response.config.url).origin !== 'https://donderhiroba.jp'){//동더히로바가 아님
+        return false;
+    }
+
+    return true;
+}
