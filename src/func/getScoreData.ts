e36e653fc7c19049049c3e8bfee4072b272553c0
HotsixMan <hotsixman05@gmail.com>
2024-06-22 11:55:53 +0900

    getScoreData 함수 수정

--- src/func/getScoreData.ts
+++ src/func/getScoreData.ts
@@ -2,7 +2,7 @@ import request from "../request.js";
 import parse from "../parse.js";
 import type { ScoreData } from "../types/scoreData.js";
 
-export default async function getScoreData(token: string, songNos: number[]): Promise<ScoreData[]> {
+export default async function getScoreData(token: string, songNos: string[]): Promise<ScoreData[]> {
     const responses = await request.requestScoreData(token, songNos);
     const parsed = responses.map(response => {
         return parse.parseScoreData(response);
