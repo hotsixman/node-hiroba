24ae5178a7268384369171bbcb4c2869919b1d8e
HotsixMan <hotsixman05@gmail.com>
2025-04-11 16:41:37 +0900

    version: 1.7.7

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import request from "./request.js";
 import parse from "./parse.js";
 import func from "./func.js";
+import checkNamcoLogin from "./parse/checkNamcoLogin.js";
+import checkLogin from "./parse/checkLogin.js";
+import HirobaError from "./hirobaError.js";
 
 const hiroba = func;
 
@@ -10,3 +13,7 @@ export {
     request,
     parse
 }
+
+export { checkNamcoLogin };
+export { checkLogin };
+export { HirobaError };
